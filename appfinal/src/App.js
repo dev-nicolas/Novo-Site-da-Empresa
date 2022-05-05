45c0fba083724147d108ecba8461d5730a4d4525
dev-nicolas <85370717+dev-nicolas@users.noreply.github.com>
2022-05-05 14:24:29 -0300

    Componente Header
    Criação do componente Header

--- appfinal/src/App.js
+++ appfinal/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React from "react"
+import "./App.css"
+import Header from "./componentes/header"
 
 export default function App(){
 
   return(
     <>
-    <h1>SITE OFICIAL FINAL</h1>
+    <Header/>
     </>
   )
 }
---
+++ appfinal/src/componentes/header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "./imgs/Logo-Branco.png"
+
+export default function header(){
+    return(
+        <section class="header">
+        <div class="logomarca">
+            <img src={Logo}/>
+        </div>
+        <div class="caixamenu">
+            <div class="Btts">
+                <button class="BotaoHeader">Entre em contato</button>
+                <button class="BotaoHeader">Abra a sua conta</button>
+            </div>
+            <div>
+                <nav>
+                    <ul class="menu">
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Sobre nós</a></li>
+                        <li><a href="">Soluções</a></li>
+                        <li><a href="">Nossos conteúdos</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        </section>
+    )
+}
