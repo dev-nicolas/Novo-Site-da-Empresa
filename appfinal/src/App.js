d0a34e226132ca0e93e947127716689d27cf3f77
dev-nicolas <dev.nicolasaugusto@gmail.com>
2022-05-09 21:44:45 -0300

    up
    up

--- appfinal/src/App.js
+++ appfinal/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./App.css"
-import Header from "./componentes/Header.js"
+import Header from "./componentes/header.js"
 import Capa from  "./Home/CapaHome"
 import Sessao2 from "./Home/Sessao2"
 import Sessao3 from "./Home/Sessao3"
